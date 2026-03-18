73947a39a33dc2a74f1525f6b0442b060b5ddc41
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-03-18 13:09:08 +0100

    modo oscuro

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,5 @@ const btn_darkM = document.getElementById("dark_mode");
 // darkMode.className="boton_oscuro"
 btn_darkM.addEventListener("click", ()=>{
     console.log("pepito")
+    btn_darkM.className="boton_oscuro"
 })
