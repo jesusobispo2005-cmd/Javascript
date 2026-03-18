b86b59779603d89dcd462dd0561b19818e138a7f
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-03-18 15:31:04 +0100

    cambiar 2 botones con un solo click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const btn_darkM = document.getElementById("dark_mode");
+const cta = document.getElementById("cta")
+
+console.log(cta)
 // darkMode.className="boton_oscuro"
 btn_darkM.addEventListener("click", ()=>{
-    console.log("pepito")
-    btn_darkM.className="boton_oscuro"
+    console.log("pepito");
+    cta.style.backgroundColor='blue';
+    btn_darkM.className="boton_oscuro";
 })
